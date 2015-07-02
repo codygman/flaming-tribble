6357d7b244d0cd64dbfd9a1d29e8123fbde0bcc2
Cody Goodman <codygman.consulting@gmail.com>
2015-07-02 05:38:42 -0500

    update javascript ffi calls

--- src/Main.js
+++ src/Main.js
@@ -4,18 +4,22 @@
 // module Main
 
 exports.withPhantom = function (cb) {
-  var phantom = require('phantom');
-  phantom.create(function (ph) {
-    if (ph !== undefined) {
-      cb(ph);
-    } else {
-      throw "couldn't create phantom";
-    }
-  });
+  return function() {
+    var phantom = require('phantom');
+    phantom.create(function (ph) {
+      if (ph !== undefined) {
+	cb(ph);
+      } else {
+	throw "couldn't create phantom";
+      }
+    });
+  }
 };
 
 exports.exitPhantom = function (ph) {
-  console.log(ph);
-  ph.exit();
+  return function() {
+    console.log(ph);
+    ph.exit();
+  }
 };
 
